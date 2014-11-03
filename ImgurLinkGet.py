a275f5d1f33999300f7f2e066bab0217cd3715b8
Martin Buck <buckmartin@buckmartin.de>
2014-11-03 21:46:27 +0100

    added class file with method names

---
+++ ImgurLinkGet.py
@@ -0,0 +1,30 @@
+import urllib
+
+class ImgurLinkGet:
+	def __init__(self):
+		self.data = None
+
+	def load_data(url):
+		pass
+
+	def get_small_squares():
+		pass
+
+	def get_big_squares():
+		pass
+
+	def get_small_thumbnails():
+		pass
+
+	def get_medium_thumbnails():
+		pass
+
+	def get_large_thumbnails():
+		pass
+
+	def get_huge_thumbnails():
+		pass
+
+	def get_originals():
+		pass
+
